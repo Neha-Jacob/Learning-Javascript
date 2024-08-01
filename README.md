187683015e9a0a6cd4e20bfa517a30971f004c72
Neha-Jacob <neha.k.jacob@gmail.com>
2024-08-01 18:40:01 +0200

    Assignment 1

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Meant for personal learning purpose
 Fundamentals1:
 - inline scripting
 - linking JS file to HTML file
+
+Assignments:
+1. console logging, linking JS file to HTML file
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is Fun!");
+ 
+if (js === "amazing") alert("Javascript is Fun!"); // popup
+
 console.log(40+23-854*23);
+
+let firstName = "John";
+let lastName = "Doe";
+console.log(firstName + " " + lastName);
